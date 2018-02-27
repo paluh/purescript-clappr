82bd3d6eeef53383e6ad3a0735aa1b96eb8555c9
Tomasz Rybarczyk <paluho@gmail.com>
2018-02-27 15:57:05 +0100

    Extract OptionsBase to ease pux integration

--- src/Clappr.purs
+++ src/Clappr.purs
@@ -7,7 +7,6 @@ import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Uncurried (EffFn1, runEffFn1)
 import DOM (DOM)
 import DOM.HTML.Types (HTMLElement)
-import Data.Array (catMaybes)
 import Data.Maybe (Maybe(..))
 import Data.Nullable (Nullable, toNullable)
 import Data.Record.Builder (build, insert)
@@ -17,10 +16,12 @@ foreign import data Clappr ∷ Type
 foreign import data CLAPPR ∷ Effect
 foreign import data Plugin ∷ Type
 
-data Parent = ParentId String | Parent HTMLElement
+-- | This base config is reused by Pux/React bindings
+type OptionsBase o = { source ∷ String | o}
 
+data Parent = ParentId String | Parent HTMLElement
 -- | Change parent so it could be only HTMLElement
-type Options = { source ∷ String, parent ∷ Parent }
+type Options = OptionsBase (parent ∷ Parent)
 
 clappr ∷ ∀ eff r. NativeOptions r → Eff (clappr ∷ CLAPPR, exception ∷ EXCEPTION, dom ∷ DOM | eff) Clappr
 clappr = runEffFn1 clapprImpl
@@ -45,6 +46,6 @@ toNativeOptions options =
   parent = case options.parent of
     ParentId p → insert _parentId (toNullable (Just p)) <<< insert _parent (toNullable Nothing)
     Parent p → insert _parentId (toNullable Nothing) <<< insert _parent (toNullable (Just p))
-  plugins = catMaybes []
+  plugins = []
 
 
