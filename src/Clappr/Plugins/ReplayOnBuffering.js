ba17dfca5a637f0409320831cd4a356f8ec30280
Tomasz Rybarczyk <paluho@gmail.com>
2018-10-01 19:27:38 +0200

    ReplayOnBuffering minor refactoring

--- src/Clappr/Plugins/ReplayOnBuffering.js
+++ src/Clappr/Plugins/ReplayOnBuffering.js
@@ -10,35 +10,42 @@ exports.replayOnBuffering = clappr.ContainerPlugin.extend({
     this.listenTo(this.container, clappr.Events.CONTAINER_STATE_BUFFERING, this.onBuffering);
     this.listenTo(this.container, clappr.Events.CONTAINER_STATE_BUFFERFULL, this.onBufferfull);
     this.listenTo(this.container, clappr.Events.CONTAINER_STOP, this.onStop);
-    this.bufferingTimeout = null;
+    this.bufferingMonitor = null;
+    //  This option can't be null/undefined when you use PureScript API.
+    //  Falling back to default value to backup pure JavaScript usage.
+    this.bufferingTimeout = this.options.replayBufferingTimeout || 60;
   },
-  _clearChecker: function() {
-    // I'm not sure how to debug print from plugins:
+  _clearMonitor: function() {
+    // I'm not sure how to debug print from plugins.
+    // How should I perform something like this:
     // clappr.Log.debug("Resetting buffering checker...");
-    if(this.bufferingTimeout !== null) {
-      clearInterval(this.bufferingTimeout);
-      this.bufferingTimeout = null;
+    // So I can stop uncommenting this when I want to debug:
+    // console.log("Resetting buffering checker...");
+    if(this.bufferingMonitor !== null) {
+      clearInterval(this.bufferingMonitor);
+      this.bufferingMonitor = null;
     }
   },
   onBufferfull: function() {
-    this._clearChecker();
+    this._clearMonitor();
   },
   onBuffering: function() {
     var bufTime = 0;
     var that = this;
-    this._clearChecker();
+    this._clearMonitor();
     // clappr.Log.debug("Starting buffering checker...");
-    this.bufferingTimeout = setInterval(function () {
+    this.bufferingMonitor = setInterval(function () {
       bufTime += 1;
       // clappr.Log.debug("Buffering checker time:" + bufTime);
-      if (bufTime >= that.options.replayBufferingTimeout) {
-        that._clearChecker();
+      // console.log("Buffering checker time:" + bufTime);
+      if (bufTime >= that.bufferingTimeout) {
+        that._clearMonitor();
         that.container.stop();
         that.container.play();
       }
     }, 1000);
   },
   onStop: function() {
-    this._clearChecker();
+    this._clearMonitor();
   }
 });
