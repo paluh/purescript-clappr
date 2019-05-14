e25d598e6ec0300e59265164df2daf31b3eac349
Tomasz Rybarczyk <paluho@gmail.com>
2019-05-14 13:11:46 +0200

    Add docs for examples

--- bin/examples.sh
+++ bin/examples.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-function build() {
-  echo "Building examples/$1/ ..."
-  pulp build -I examples/$1/src --build-path examples/$1/output
-  webpack --config examples/$1/webpack.config.js
-}
-
-build simple
-
-build plugins
-
-build events
+make
 
 python -m http.server
